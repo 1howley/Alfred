2f7136acb27cda652e80f4fbe7dcc98f17aee0cc
Ana Paula Ferreira <bielgabrielfs61@gmail.com>
2023-11-02 18:54:05 -0300

    first camera code

--- src/classes/camera.py
+++ src/classes/camera.py
@@ -1,20 +1,47 @@
-import os
-from PIL import Image
+import cv2
+import mediapipe as mp
 
-# Defina o diretório da pasta onde suas imagens estão localizadas
-pasta_imagens = 'D:\Codigos\Alfred\src\classes\Pictures'
+# Carregar a imagem de referência para comparação
+reference_image = cv2.imread('caminho/para/sua/imagem_de_referencia.jpg')
 
-# Lista de arquivos na pasta
-arquivos = os.listdir(pasta_imagens)
+# Inicializar o módulo MediaPipe FaceMesh
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+face_detection = mp_face_detection.FaceDetection()
 
-# Filtra os arquivos para pegar apenas os que têm extensão de imagem
-imagens = [arquivo for arquivo in arquivos if arquivo.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
+# Inicializar a captura de vídeo da webcam (0 representa a câmera padrão)
+video_capture = cv2.VideoCapture(0)
 
-# Carrega cada imagem e adiciona ao array
-array_de_imagens = []
-for imagem in imagens:
-    caminho_completo = os.path.join(pasta_imagens, imagem)
-    imagem_carregada = Image.open(caminho_completo)
-    array_de_imagens.append(imagem_carregada)
+while True:
+    # Capturar frame a frame
+    ret, frame = video_capture.read()
 
-# Agora você tem um array de imagens prontas para uso
+    # Detectar rostos na imagem atual
+    result = face_detection.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+    if result.detections:
+        for detection in result.detections:
+            bboxC = detection.location_data.relative_bounding_box
+            ih, iw, _ = frame.shape
+            x, y, w, h = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                         int(bboxC.width * iw), int(bboxC.height * ih)
+
+            # Comparar com a imagem de referência (simplificado para detecção)
+            reference_height, reference_width, _ = reference_image.shape
+            if h == reference_height and w == reference_width:
+                print("Rosto detectado. Esta pessoa está no banco de dados.")
+            else:
+                print("Rosto detectado. Esta pessoa não está no banco de dados.")
+
+            # Desenhar o retângulo ao redor do rosto detectado
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # Exibir o frame resultante
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Liberar o vídeo e fechar as janelas
+video_capture.release()
+cv2.destroyAllWindows()
